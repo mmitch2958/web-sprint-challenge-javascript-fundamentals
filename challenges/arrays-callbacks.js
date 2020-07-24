15d42a915f8f6c430f37d8fd4d6a0dec731b4b4b
mmitch2958 <mikescottmitchell@outlook.com>
2020-07-24 12:24:43 -0400

    finsihed arrays

--- challenges/arrays-callbacks.js
+++ challenges/arrays-callbacks.js
@@ -1,6 +1,7 @@
 // ==== ADVANCED Array Methods ====
 
-// Given this zoo data from around the United States, follow the instructions below.  Use the specific array methods in the requests below to solve the problems.
+// Given this zoo data from around the United States, follow the instructions below.  
+//Use the specific array methods in the requests below to solve the problems.
 
 const zooAnimals = [
   { animal_name: "Jackal, asiatic", population: 5, scientific_name: "Canis aureus", state: "Kentucky" },
@@ -17,35 +18,63 @@ const zooAnimals = [
 
 /* Request 1: .forEach()
 
-The zoos want to display both the scientific name and the animal name in front of the habitats. Populate the displayNames array with only the animal_name and scientific_name of each animal. displayNames will be an array of strings, and each string should follow this pattern: "Name: Jackal, asiatic, Scientific: Canis aureus."
+The zoos want to display both the scientific name and the animal name in front of the habitats. Populate the displayNames array 
+with only the animal_name and scientific_name of each animal. displayNames will be an array of strings, and each string should follow this 
+pattern: "Name: Jackal, asiatic, Scientific: Canis aureus."
 
 */
-const displayNames = [];
+
+
+let displayNames = zooAnimals.map(function(item){
+  return `${item.animal_name}, ${item.scientific_name}`;
+}
+);
+
 console.log(displayNames);
 
 /* Request 2: .map()
 
-The zoos need a list of all their animal's names (animal_name only) converted to lower case. Using map, create a new array of strings named lowCaseAnimalNames, each string following this pattern: "jackal, asiatic". Log the resut.
+The zoos need a list of all their animal's names (animal_name only) converted to lower case. Using map, create a new array of 
+strings named lowCaseAnimalNames, each string following this pattern: "jackal, asiatic". Log the resut.
 
 */
+const lowCaseAnimalNames = zooAnimals.map(function(item){
+    return item.animal_name.toLowerCase();
+}
+);
+  
 
-const lowCaseAnimalNames
 console.log(lowCaseAnimalNames);
 
 /* Request 3: .filter() 
 
-The zoos are concerned about animals with a lower population count. Using filter, create a new array of objects called lowPopulationAnimals which contains only the animals with a population less than 5.
+The zoos are concerned about animals with a lower population count. Using filter, 
+create a new array of objects called lowPopulationAnimals which contains only the animals with a population less than 5.
 
 */
-const lowPopulationAnimals
-console.log(lowPopulationAnimals);
+const lowPopulationAnimals = zooAnimals.filter(function(item){
+  
+    return item.population < 5;
+  });
+  
+  console.log(lowPopulationAnimals);
+  
+  const lowPopAnimals = lowPopulationAnimals.map(function(item){
+    return item.animal_name;
+  });
+  
+  console.log(lowPopAnimals);
 
 /* Request 4: .reduce() 
 
-The zoos need to know their total animal population across the United States. Find the total population from all the zoos using the .reduce() method. Remember the reduce method takes two arguments: a callback (which itself takes two args), and an initial value for the count.
+The zoos need to know their total animal population across the United States. Find the total population from all the zoos
+ using the .reduce() method. Remember the reduce method takes two arguments: 
+ a callback (which itself takes two args), and an initial value for the count.
 
 */
-let populationTotal = 0;
+let populationTotal = zooAnimals.reduce(function (accum, item, index) {
+  return accum + item.population;
+}, 0);
 console.log(populationTotal);
 
 
@@ -58,12 +87,22 @@ console.log(populationTotal);
   * The consume function should return the invocation of cb, passing a and b into cb as arguments
 */
 
+function consume(a, b, cb){
+ return cb(a, b); }
+
+
+
+
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
+const add = (num1, num2) => num1 + num2;
+const multiply = (num1, num2) => num1 * num2;
+const greeting = ("first", "last") => `Hello ${this.first} ${this.last}, nice to meet you !`
+
 
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
@@ -71,7 +110,9 @@ console.log(populationTotal);
 // console.log(consume(10, 16, multiply)); // 160
 // console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
-
+console.log(consume(2, 2, add));
+console.log(consume(10, 16, multiply));
+console.log(consume ('mary', 'Poppins', greeting))
 
 
 /*
