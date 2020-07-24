140d5e25479da49e2db0476a20b10910e43ed400
mmitch2958 <mikescottmitchell@outlook.com>
2020-07-24 12:41:53 -0400

    done clossures

--- challenges/closure.js
+++ challenges/closure.js
@@ -18,7 +18,15 @@ myFunction();
 
 // Explanation: 
 
+// Becuaes the variable Internal is in nestedFunctions scope. 
 
 /* Task 2: Counter */
 
-/* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+/* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, 
+'summation(4)` should return 10 because 1+2+3+4 is 10. */
+
+summation(num1){
+  for (let i = 0; i < num1.length; i++)
+    return i + 1
+}
+console.log(summation(4));
